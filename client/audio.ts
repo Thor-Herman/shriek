a4f6b146c22f414ebb8041bf08f7c3c32eade5b8
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-03-23 20:51:48 +0100

    Create AskMicrophonePermission

--- client/audio.ts
+++ client/audio.ts
@@ -6,6 +6,18 @@ import workletURL from "./lib/volume-worklet.js?url";
 
 export async function askMicrophonePermission(
   onVolume: (volume: number) => void
-) {}
+) {
+  const media = await navigator.mediaDevices.getUserMedia({audio: true});
+  const audioContext = new AudioContext();
+  const stream = audioContext.createMediaStreamSource(media);
+
+  await audioContext.audioWorklet.addModule(workletURL);
+  const workletNode = new AudioWorkletNode(audioContext, 'volumeworklet');
+  workletNode.port.onmessage = (message) => {
+    onVolume(message.data.volume);
+  };
+
+  stream.connect(workletNode).connect(audioContext.destination);
+}
 
 export default askMicrophonePermission;
