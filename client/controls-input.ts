7a86e3eb54217b5fd665784d4159fd9a8e1f6323
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-03-23 20:51:59 +0100

    Add controls input

--- client/controls-input.ts
+++ client/controls-input.ts
@@ -1,4 +1,18 @@
 ////////////////////////////////////////
 // Lag en ControlInput modul som holder tilstand på venstre/høyre.
 ////////////////////////////////////////
-export default function controlsInput() {}
+const LEFT = 'a';
+const RIGHT = 'd';
+
+export default function controlsInput() {
+    const controls = {left: false, right: false};
+
+    document.addEventListener("keydown", (ev: KeyboardEvent) => {
+        controls.left = ev.key === LEFT;
+        controls.right = ev.key === RIGHT;
+    });
+    document.addEventListener("keyup", () => {
+        controls.left = controls.right = false;
+    });
+    return controls;
+};
